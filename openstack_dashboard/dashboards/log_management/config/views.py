11de97388bb1f5acb4ff6d5fe6001587d84789e7
vanduc95 <ducbac230895@gmail.com>
2016-10-12 22:42:11 +0700

    create form config color and handle

--- openstack_dashboard/dashboards/log_management/config/views.py
+++ openstack_dashboard/dashboards/log_management/config/views.py
@@ -11,6 +11,7 @@
 # under the License.
 
 from horizon import views
+from openstack_dashboard.dashboards.log_management.config.file_config import handle_file_color
 
 
 class IndexView(views.APIView):
@@ -19,4 +20,27 @@ class IndexView(views.APIView):
 
     def get_data(self, request, context, *args, **kwargs):
         # Add data to the context here...
+        return context
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        try:
+            if self.request.method == 'GET':
+                if 'save' in self.request.GET:
+                    error = self.request.GET['ERROR']
+                    info = self.request.GET['INFO']
+                    warn = self.request.GET['WARN']
+                    debug = self.request.GET['DEBUG']
+                    handle_file_color.write_file(error, info, warn, debug)
+                elif 'default' in self.request.GET:
+                    handle_file_color.write_file("#fc0404", "#4df919", "#faf204", "#707b7a")
+        except Exception:
+            pass
+
+        color = handle_file_color.read_file()
+        context['ERROR'] = color[0].split()[1]
+        context['INFO'] = color[1].split()[1]
+        context['WARN'] = color[2].split()[1]
+        context['DEBUG'] = color[3].split()[1]
+
         return context
