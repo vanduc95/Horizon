36990ce43ab2eee9ad6f6344a91d40f2db31c01e
huynhducbk95 <huynhducpro95@gmail.com>
2016-11-06 16:14:22 +0700

    create service handle scale

---
+++ openstack_dashboard/dashboards/docker_swarm/docker_service/scale_services/service.py
@@ -0,0 +1,32 @@
+
+
+
+# from django.http.response import HttpResponse
+from docker import Client
+import docker
+
+
+def scale(serviceID,numReplicas):
+    cli = Client(base_url="unix://var/run/docker.sock")
+    service = cli.inspect_service(serviceID)
+    service_index = service['Version']['Index']
+    replicas = service['Spec']['Mode']['Replicated']['Replicas']
+    tasktamplate = service['Spec']['TaskTemplate']
+    container = tasktamplate['ContainerSpec']
+
+    taskObj = docker.types.TaskTemplate(container)
+
+    if (numReplicas==None):
+        try:
+            cli.update_service(serviceID, service_index, task_template=taskObj,
+                           mode={'Replicated': {'Replicas': replicas+1}})
+            return True
+        except Exception :
+            return False
+    else:
+        try:
+            cli.update_service(serviceID, service_index, task_template=taskObj,
+                           mode={'Replicated': {'Replicas': numReplicas}})
+            return True
+        except Exception :
+            return False
